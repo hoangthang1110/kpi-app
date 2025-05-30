d74a6d19810dac5860033208d2b9b7ea4b883eaf
hoangthang1110 <hoangthang1110@gmail.com>
2025-05-30 17:45:57 +0700

    Delete utils directory

--- utils/docx_reader.py
+++
@@ -1,9 +0,0 @@
-from docx import Document
-
-def read_docx_text(filepath: str) -> str:
-    """
-    Đọc nội dung text từ file .docx
-    """
-    doc = Document(filepath)
-    paragraphs = [para.text.strip() for para in doc.paragraphs if para.text.strip()]
-    return "\n".join(paragraphs)
